eebd678ffcc8bf903b23d2dfdbad9f8b67eb7a14
moiz <moiziqbal39@gmaial.com>
2024-01-24 14:00:48 +0500

    add cors origin,method,creds

--- server/index.js
+++ server/index.js
@@ -5,7 +5,11 @@ const cors = require("cors");
 const port = process.env.PORT || 5000;
 
 //midleWare
-app.use(cors());
+app.use(cors({
+  origin:["https://book-world-frontend.vercel.app/"],
+  methods:["GET","POST"],
+  credentials:true, 
+}));
 app.use(express.json());
 
 app.get("/", (req, res) => {
